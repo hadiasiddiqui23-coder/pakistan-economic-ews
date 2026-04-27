796a9509b644476cf76ab9394b6df7023e910c4a
hadiasiddiqui23-coder <hadiasiddiqui23@gmail.com>
2026-04-27 23:29:06 +0500

    Add files via upload

--- data_fetcher.py
+++ data_fetcher.py
@@ -32,20 +32,20 @@ def get_historical_inflation():
         response = requests.get(url)
         data = response.json()
         years = [item['date'] for item in data[1]][::-1] # Reverse to go old -> new
-        values = [item['value'] for item in data[1]][::-1]
+        values = [item['value'] if item['value'] is not None else 0 for item in data[1]][::-1]
         return years, values
     except:
         return ["2020", "2021", "2022", "2023", "2024"], [10, 12, 25, 30, 15]
-    
-    def get_historical_data(indicator_code):
-        """Fetches 10 years of data for any World Bank indicator code."""
-    # The URL now uses the 'indicator_code' we pass to it
-    url = f"https://api.worldbank.org/v2/country/PK/indicator/{indicator_code}?format=json&per_page=10"
+
+# --- FIX: This function was accidentally indented inside the one above! ---
+def get_historical_data(indicator_code):
+    """Fetches 10 years of data for any World Bank indicator code."""
+    url = f"https://api.worldbank.org/v2/country/PAK/indicator/{indicator_code}?format=json&per_page=10"
     try:
         response = requests.get(url)
         data = response.json()
         
-        # We reverse the data so it goes from oldest year to newest year
+        # Grab years and values, replacing None with 0 so the graph doesn't break
         years = [item['date'] for item in data[1]][::-1]
         values = [item['value'] if item['value'] is not None else 0 for item in data[1]][::-1]
         
